994ce5db56a4da25745ec65ae75553a543a8ca8a
Notan23 <notan.vokepluhs@gmail.com>
2016-04-06 15:44:50 +0300

    initial commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://greenpartner.loc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://greenpartner.loc/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/work/greenpartner/www/catalog/');
+define('DIR_SYSTEM', 'D:/work/greenpartner/www/system/');
+define('DIR_LANGUAGE', 'D:/work/greenpartner/www/catalog/language/');
+define('DIR_TEMPLATE', 'D:/work/greenpartner/www/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/work/greenpartner/www/system/config/');
+define('DIR_IMAGE', 'D:/work/greenpartner/www/image/');
+define('DIR_CACHE', 'D:/work/greenpartner/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/work/greenpartner/www/system/storage/download/');
+define('DIR_LOGS', 'D:/work/greenpartner/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/work/greenpartner/www/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/work/greenpartner/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', '123456Qwerty');
+define('DB_DATABASE', 'opencart_new');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
